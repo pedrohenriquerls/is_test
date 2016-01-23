3887c6292f57ffd93677350bdf6d103ab99970a3
Pedro Henrique Rodrigues <pedrohenriquerls@gmail.com>
2016-01-23 16:20:51 -0200

    classroom spec

--- spec/model/classroom_spec.rb
+++ spec/model/classroom_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe Classroom do
 
+  subject(:student){Student.create(name: 'Joãozinho', status: Student::STATUS[0], register_number: '123123')}
+  subject(:course){Course.new(name: 'Inglês', status: Course::STATUS[0], description: 'Aula de inglês gratuita')}
+
+  it 'should be create the classroom with course and student' do
+    classroom = Classroom.create student: student, course: course
+
+    classroom.entry_at.should be
+    student.courses.first.should eq course
+    course.students.first.should eq student
+  end
 end
